d893d647d04b23d06b58e8094d34a795c53033be
Alexander Drapkin <alexander.drapkin@outlook.com>
2023-06-23 00:18:36 +0300

    add antd config provider with heIL locale and directoin of rtl

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ConfigProvider } from 'antd';
+import heIL from 'antd/locale/he_IL';
+
+import App from './App.tsx';
+
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider locale={heIL} direction="rtl">
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
