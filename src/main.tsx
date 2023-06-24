0be0b26a8e3c06f0fba7ee9cedefa5166c23d335
Alexander Drapkin <alexander.drapkin@outlook.com>
2023-06-24 15:43:18 +0300

    delete index.css because now antd will set the styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import CustomAntdProvider from '@features/antd/CustomAntdProvider.tsx';
 
 import App from './App.tsx';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CustomAntdProvider>
