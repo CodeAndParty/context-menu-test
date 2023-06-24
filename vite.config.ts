038a5201779bc9a92fedf67a832ece21b9fa493c
Alexander Drapkin <alexander.drapkin@outlook.com>
2023-06-24 14:55:50 +0300

    add features type alias

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components'),
+      '@features': path.resolve(__dirname, './src/features'),
     },
   },
 });
